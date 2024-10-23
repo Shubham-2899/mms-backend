2aebce9bf2696587cb0a934257bdf1e71edd4b08
Shubham-2899 <shubhamlatake123@gmail.com>
2024-10-23 22:34:04 +0530

    increased payload size

--- dist/main.js
+++ dist/main.js
@@ -3,10 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const bullmq_dashboard_service_1 = require("./bullmq-dashboard/bullmq-dashboard.service");
+const express = require("express");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule, {
         cors: true,
     });
+    app.use(express.json({ limit: '5mb' }));
+    app.use(express.urlencoded({ limit: '5mb', extended: true }));
     const bullmqDashboardService = app.get(bullmq_dashboard_service_1.BullmqDashboardService);
     bullmqDashboardService.bindMiddleware(app);
     await app.listen(5000);
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BullmqDashboardService } from './bullmq-dashboard/bullmq-dashboard.service';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
+
+  // Increase payload limit
+  app.use(express.json({ limit: '5mb' }));
+  app.use(express.urlencoded({ limit: '5mb', extended: true }));
 
   // Get BullMQ service instance to attach middleware
   const bullmqDashboardService = app.get(BullmqDashboardService);
